4ed5da9472c7dcdc927e846f59b7e9ef0256d53d
SperepS <carni@JESPTOP.com>
2022-12-21 18:36:31 +0100

    added new endpoint: getUserProfile

--- service/database/get-user-profile.go
+++ service/database/get-user-profile.go
@@ -1,18 +1,30 @@
 package database
 
+import "log"
+
 func (db *appdbimpl) GetUserProfile(username string) ([]Profile, error) {
 	var Profiles []Profile
-	rows, err := db.c.Query("SELECT username,id FROM users WHERE username LIKE ?", username)
+	rows, err := db.c.Query("SELECT id,username FROM users WHERE username LIKE ?", "%"+username+"%")
+	log.Println(rows)
 	if err != nil {
 		return Profiles, err
 	}
+
 	for rows.Next() {
 		p := Profile{}
-		err = rows.Scan(&p.Username, &p.Id)
+
+		err = rows.Scan(&p.Id, &p.Username)
+		if err != nil {
+			return Profiles, err
+		}
+
 		var Photos []Photo
+
 		Photos, err = db.GetPhotosbyId(int(p.Id))
+
 		p.Photos = Photos
 		Profiles = append(Profiles, p)
+		log.Println(p.Photos)
 	}
 
 	return Profiles, err
